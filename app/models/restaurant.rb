d245779354610a1aa8c54e5f5b5f1e297a1b6fbd
Antoine Vaissie <hello@antoinev.com>
2023-01-03 22:00:17 +0100

    pablo

---
+++ app/models/restaurant.rb
@@ -0,0 +1,5 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :phone_number, :address, :name, presence: true
+  validates :category, presence: true, inclusion: { in: %w(french japanese belgian chinese italian)}
+end
